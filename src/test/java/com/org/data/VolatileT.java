26043fd098b7e8a4d23ab2dff629db379a41eb72
huajun.wang01 <2452255928@qq.com>
2018-12-22 17:07:18 +0800

    multithreading first

---
+++ src/test/java/com/org/data/VolatileT.java
@@ -0,0 +1,31 @@
+package com.org.data;
+
+/**
+ * Created by huajun.wang01 on 2018/12/16.
+ */
+public class VolatileT {
+    public static volatile int race = 0;
+
+    public static void increase() {
+        race++;
+    }
+
+    private static final int THREADS_COUNT = 2;
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[THREADS_COUNT];
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            threads[i] = new Thread(new Runnable() {
+                public void run() {
+                    for (int i = 0; i < 10000; i++) {
+                        increase();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+        while (Thread.activeCount() > 1)
+            Thread.yield();
+        System.out.println(race);
+    }
+}
